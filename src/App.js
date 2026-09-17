3a873461f27951985be7a8f6d8956b79e563bc26
hoahieplth <lethihoahiep95@gmail.com>
2026-09-17 13:14:37 +0700

    hoàn thiện phần Header (Phần 2)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
-        <Router>
+        <Router
+            future={{
+                v7_startTransition: true,
+                v7_relativeSplatPath: true,
+            }}
+        >
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -49,7 +49,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                                 }}
                             />
                         )}
-                        {renderItem()}
+                        <div className={cx('menu-body')}>{renderItem()}</div>
                     </PopperWrapper>
                 </div>
             )}
